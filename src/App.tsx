b3a698cc3027daea19534ec56a81a476021baf9f
lintricker <alina.ishem01@gmail.com>
2023-04-03 15:42:52 +0400

    addButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { AdminPage } from './Components/admin/adminPage';
-import { Auth } from './Components/admin/auth/Auth';
 
 function App() {
   return (
